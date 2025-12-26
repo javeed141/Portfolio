0af15cad40f8c2e04e8c198959dbfe95ee44958c
javeed141 <javeedshaik7346@gmail.com>
2025-12-26 15:54:29 +0530

    changed resume

--- src/components/pages/HeroSection.jsx
+++ src/components/pages/HeroSection.jsx
@@ -121,7 +121,7 @@ Full Stack Developer 🧑‍💻 | Building Fast, Reliable Web Apps ⚡
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1D8DMXc4bCM2N8uT0lff63XiWR4D4Khpa/view"
+                href="https://drive.google.com/file/d/1CpeAWvf03cgkMUhtimVOOr0NNVXbKt1n/view?usp=drive_open"
                 target="_blank"
                 className={cls(
                   "inline-flex items-center justify-center gap-2 px-4 py-3 rounded-full font-semibold w-full sm:w-auto",
